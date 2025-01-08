c42a5697a585ec4d3224b1da2b78051a00946da2
unknown <kyawzinhein162@gmail.com>
2025-01-08 15:16:57 +0630

    before ui only

--- Server/routes/customer.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const customerController = require("../controllers/customers");
-
-// POST /get-invoices
-router.post("/", customerController.createCustomer);
-
-module.exports = router;
--- Server/routes/invoices.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const inVoiceController = require("../controllers/invoices");
-
-// POST /get-invoices
-router.post("/", inVoiceController.createInvoice);
-
-module.exports = router;
